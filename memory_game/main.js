86e7c33dadaf9329a45fe6395f90381a99780d2c
JSriwongsa <sriwongsa.jtp@gmail.com>
2023-06-15 18:44:03 -0500

    added javaScript file

---
+++ memory_game/main.js
@@ -0,0 +1,8 @@
+const toMove = document.getElementById('moving');
+const timer = document.getElementById('time');
+const pauseBtn = document.getElementById('pause');
+const startBtn = document.getElementById('start');
+const seeResult = document.getElementById('result');
+const playContainer = document.querySelector('.play-container');
+const run = document.querySelector('.run-container');
+
